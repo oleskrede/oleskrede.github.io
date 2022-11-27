d7bb27fd92d2bfa25cf2ef10ad69f5ee1e384850
Ole Steinar L. Skrede <ole.skrede+github@pm.me>
2022-11-27 16:24:46 +0100

    typescript phaser template

---
+++ untitled-zombie-game/src/game.ts
@@ -0,0 +1,29 @@
+import 'phaser'
+import MainScene from './scenes/mainScene'
+
+const DEFAULT_WIDTH = 1280
+const DEFAULT_HEIGHT = 720
+
+const config = {
+  type: Phaser.AUTO,
+  backgroundColor: '#ffffff',
+  scale: {
+    parent: 'phaser-game',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT
+  },
+  scene: [MainScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: { y: 400 }
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  new Phaser.Game(config)
+})
